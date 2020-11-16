8f88c43be9151050954bc19f280630c35b6530ff
furps <lukasznowacki@mail.com>
2020-11-16 14:41:17 +0100

    Adding form with validation to the home page
    Via form user choose which request sends to govermental API
    Via form user pass data which will be send in request

--- src/main/java/com/whitelist/taxpayers/controller/AppController.java
+++ src/main/java/com/whitelist/taxpayers/controller/AppController.java
@@ -1,12 +1,31 @@
 package com.whitelist.taxpayers.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.whitelist.taxpayers.model.Form;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+
+@Controller
 public class AppController {
-	
+
 	@GetMapping("/")
 	public String getIndex() {
 		return "index";
 	}
 
+	@PostMapping("/search")
+	public String search(@ModelAttribute("form") @Valid Form form, BindingResult bindingResult, Model model) {
+		if (bindingResult.hasErrors()) {
+			model.addAttribute("form", form);
+			return "index";
+		}
+		return "index";
+	}
 }
