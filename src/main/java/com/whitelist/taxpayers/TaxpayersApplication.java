a5cce2824b083fc5a32088f1fc477237603fae0b
furps <lukasznowacki@mail.com>
2020-11-16 10:50:55 +0100

    init

---
+++ src/main/java/com/whitelist/taxpayers/TaxpayersApplication.java
@@ -0,0 +1,13 @@
+package com.whitelist.taxpayers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxpayersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxpayersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/whitelist/taxpayers/TaxpayersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whitelist.taxpayers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxpayersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
