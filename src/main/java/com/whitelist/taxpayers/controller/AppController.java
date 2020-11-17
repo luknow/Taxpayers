0202663e7370210dd183b4d974944f3429b5377a
furps <lukasznowacki@mail.com>
2020-11-17 17:33:16 +0100

    Adding API response to view
    Adding API results response to view if request was valid
    Adding API error response to view if request was invalid

--- src/main/java/com/whitelist/taxpayers/controller/AppController.java
+++ src/main/java/com/whitelist/taxpayers/controller/AppController.java
@@ -3,12 +3,20 @@ package com.whitelist.taxpayers.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.client.HttpClientErrorException.BadRequest;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.whitelist.taxpayers.api.EntityListResponse;
 import com.whitelist.taxpayers.model.Form;
+import com.whitelist.taxpayers.service.ApiService;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.validation.Valid;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -23,6 +31,12 @@ public class AppController {
 	private final String NIP_SEARCH_METHOD = "Nip";
 	private final String REGON_SEARCH_METHOD = "Regon";
 
+	private ApiService apiService;
+
+	public AppController(ApiService apiService) {
+		this.apiService = apiService;
+	}
+
 	@GetMapping("/")
 	public String getIndex(Model model) {
 		model.addAttribute("form", new Form());
@@ -36,6 +50,7 @@ public class AppController {
 		String number = "";
 		String searchMethod = "";
 		int numberLength = 0;
+		ResponseEntity<EntityListResponse> responseEntity = null;
 
 		if (bindingResult.hasErrors()) {
 			model.addAttribute("form", form);
@@ -54,6 +69,15 @@ public class AppController {
 
 		if (number.matches("^[0-9]{" + numberLength + "}$") == false) {
 			redirectAttributes.addFlashAttribute("danger", "Podano błędny numer!");
+			return "redirect:/";
+		}
+
+		try {
+			responseEntity = sendRequestToGovernmentalAPI(searchMethod, number);
+			responseEntity.getBody().getResult().setDate(getCurrentDate());
+			model.addAttribute("results", responseEntity.getBody().getResult());
+		} catch (BadRequest e) {
+			redirectAttributes.addFlashAttribute("danger", e.getLocalizedMessage().split("\"")[7]);
 			return "redirect:/";
 		}
 		return "index";
@@ -76,7 +100,31 @@ public class AppController {
 		default:
 			numberLength = -1;
 		}
-		
+
 		return numberLength;
+	}
+
+	public String getCurrentDate() {
+		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		Date date = new Date();
+		return dateFormat.format(date);
+	}
+
+	public ResponseEntity<EntityListResponse> sendRequestToGovernmentalAPI(String searchMethod, String number) {
+
+		ResponseEntity<EntityListResponse> responseEntity = null;
+
+		switch (searchMethod) {
+		case BANK_ACCOUNT_SEARCH_METHOD:
+			responseEntity = apiService.bankAccountdate(number, getCurrentDate());
+			break;
+		case NIP_SEARCH_METHOD:
+			responseEntity = apiService.nipsdate(number, getCurrentDate());
+			break;
+		case REGON_SEARCH_METHOD:
+			responseEntity = apiService.regonsdate(number, getCurrentDate());
+			break;
+		}
+		return responseEntity;
 	}
 }
